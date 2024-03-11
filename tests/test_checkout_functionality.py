b2b41df78ee0e47b05def399926749e95c7e9b89
Oleksandra <Oleksandra@ekfc.ae>
2024-03-11 12:13:32 +0200

    Added some changes to test

--- tests/test_checkout_functionality.py
+++ tests/test_checkout_functionality.py
@@ -9,13 +9,13 @@ from conftest import driver
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-FIRSTNAME = "Kate"
-LASTNAME = "Parker"
-POSTCODE = "12345"
+first_name = "Kate"
+last_name = "Parker"
+postcode = "12345"
 
 
 class TestCheckoutProcess:
-    def test_checkout_functionality(self, login_to_saucedemo, add_profuct_to_saucedome):
+    def test_checkout_functionality(self, login_to_saucedemo, add_product_to_bucket):
         checkout_button = driver.find_element(By.XPATH, '//button[@id="checkout"]')
         assert checkout_button.is_displayed(), "The button is not visible"
         checkout_button.click()
@@ -23,21 +23,21 @@ class TestCheckoutProcess:
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "checkout_info_container")))
         first_name_field = driver.find_element(By.XPATH, '//input[@id="first-name"]')
         assert first_name_field.is_displayed(), "The first name is not visible"
-        first_name_field.send_keys(FIRSTNAME)
+        first_name_field.send_keys(first_name)
         current_first_name_value = first_name_field.get_attribute("value")
-        assert current_first_name_value == FIRSTNAME, "The first name is not displayed correctly.."
+        assert current_first_name_value == first_name, "The first name is not displayed correctly.."
 
         last_name_field = driver.find_element(By.XPATH, '//input[@id="last-name"]')
         assert last_name_field.is_displayed(), "The last name is not visible"
-        last_name_field.send_keys(LASTNAME)
+        last_name_field.send_keys(last_name)
         current_last_name_value = last_name_field.get_attribute("value")
-        assert current_last_name_value == LASTNAME, "The last name is not displayed correctly"
+        assert current_last_name_value == last_name, "The last name is not displayed correctly"
 
         post_code_field = driver.find_element(By.XPATH, '//input[@id="postal-code"]')
         assert post_code_field.is_displayed(), "The post code is not visible"
-        post_code_field.send_keys(POSTCODE)
+        post_code_field.send_keys(postcode)
         current_post_code_value = post_code_field.get_attribute("value")
-        assert current_post_code_value == POSTCODE, "The post code is not displayed"
+        assert current_post_code_value == postcode, "The post code is not displayed"
 
         continue_button = driver.find_element(By.XPATH, '//input[@id="continue"]')
         assert continue_button.is_displayed(), "The continue button is not visible"
@@ -49,8 +49,10 @@ class TestCheckoutProcess:
         finish_button.click()
 
         header = driver.find_element(By.XPATH, '//*[@id="checkout_complete_container"]/h2')
-        assert header.is_displayed(), "Order confirmation message not found"
+        assert header.is_displayed(), "Order confirmation message does not found"
+        confirmation_message = header.text
+        assert "Thank you for your order!" in confirmation_message, "Order confirmation message does not match"
 
-        logging.info("Test4 passed: THANK YOU FOR YOUR ORDER a confirmation message is displayed.")
+        logging.info("Test4 passed: Thank you for your order a confirmation message is displayed.")
 
         driver.close()
