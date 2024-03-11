194bd7f484a0e61abda234be3271ee6d35410011
Sv-24 <48556135+Sv-24@users.noreply.github.com>
2024-03-11 12:36:29 +0200

    test_logout_functionality (#7)
    * test_logout_functionality
    
    * Add some changes
    
    * Add some changes
    
    * Delete tests/conftest.py
    
    * Delete tests/test_checkout_functionality.py
    
    * Add some changes
    
    * Add some changes
    
    * Added some changes to test
    
    * Changed logout test
    
    * removed fixture from test
    
    ---------
    
    Co-authored-by: Oleksandra <Oleksandra@ekfc.ae>
    Co-authored-by: iryna_lavrinok <iryna.lavrinok@pango.co>

--- tests/test_logout_functionality.py
+++ tests/test_logout_functionality.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,16 +8,13 @@ from conftest import driver
 
 
 class TestLogoutPage:
-
     def test_logout_functionality(self, login_to_saucedemo):
-        expected_product_page_url = "https://www.saucedemo.com/inventory.html"
-        current_url = driver.current_url
-        assert current_url == expected_product_page_url, "User is not redirected to the product page."
-
         menu_button = driver.find_element(By.XPATH, '//button[@id="react-burger-menu-btn"]')
+        assert menu_button.is_displayed(), "The button is not visible"
         menu_button.click()
 
         logout_button = driver.find_element(By.XPATH, '//a[@id="logout_sidebar_link"]')
+        assert logout_button.is_displayed(), "The button is not visible"
         logout_button.click()
 
         WebDriverWait(driver, 10).until(EC.url_to_be("https://www.saucedemo.com/"))
@@ -24,6 +23,9 @@ class TestLogoutPage:
         driver.get(restricted_page_url)
         WebDriverWait(driver, 10).until(EC.url_to_be("https://www.saucedemo.com/"))
 
-        print("Test passed:User successfully log out of the application")
+        header = driver.find_element(By.XPATH, '//div[@class="login_logo"]')
+        assert header.is_displayed(), "Page header is not displayed."
+
+        logging.info("Test passed:User successfully log out of the application.")
 
         driver.close()
