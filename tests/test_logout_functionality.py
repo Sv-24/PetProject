92b152d075cebe46927a1cdc924af90b2f654867
iryna_lavrinok <iryna.lavrinok@pango.co>
2024-03-11 12:33:10 +0200

    removed fixture from test

--- tests/test_logout_functionality.py
+++ tests/test_logout_functionality.py
@@ -1,6 +1,5 @@
 import logging
 
-import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,14 +8,6 @@ from conftest import driver
 
 
 class TestLogoutPage:
-
-    @pytest.fixture
-    def login_to_saucedemo(self, login_to_saucedemo):
-        expected_product_page_url = "https://www.saucedemo.com/inventory.html"
-        current_url = driver.current_url
-        assert current_url == expected_product_page_url, "User is not redirected to the product page."
-        return login_to_saucedemo
-
     def test_logout_functionality(self, login_to_saucedemo):
         menu_button = driver.find_element(By.XPATH, '//button[@id="react-burger-menu-btn"]')
         assert menu_button.is_displayed(), "The button is not visible"
