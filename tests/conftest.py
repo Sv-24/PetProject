69366c6100c53d09d69df51b4d5d01503f90ecbe
Sv-24 <48556135+Sv-24@users.noreply.github.com>
2024-03-11 12:25:30 +0200

    test_checkout_functionality (#6)
    * test_checkout_functionality
    
    * Added some changes to test
    
    ---------
    
    Co-authored-by: Oleksandra <Oleksandra@ekfc.ae>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
@@ -26,20 +26,22 @@ def login_to_saucedemo():
     login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
     login_button.click()
 
+    expected_product_page_url = "https://www.saucedemo.com/inventory.html"
+    current_url = driver.current_url
+    assert current_url == expected_product_page_url, "User is not redirected to the product page"
+
 
 @pytest.fixture
-def add_profuct_to_saucedome():
+def add_product_to_bucket():
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "inventory_list")))
-    add_button = driver.find_element(By.XPATH,'//button[@id="add-to-cart-sauce-labs-backpack"]')
+    add_button = driver.find_element(By.XPATH, '//button[@id="add-to-cart-sauce-labs-backpack"]')
     add_button.click()
 
     WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH, '//div[@id="shopping_cart_container"]')))
-    container_button = driver.find_element(By.XPATH,'//div[@id="shopping_cart_container"]')
+    container_button = driver.find_element(By.XPATH, '//div[@id="shopping_cart_container"]')
     container_button.click()
 
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="cart_list"]')))
     item = driver.find_element(By.XPATH, '//div[@class="inventory_item_name" and text() = "Sauce Labs Backpack"] ')
     assert item.is_displayed(), " item is not displayed in the cart_list."
-
-
